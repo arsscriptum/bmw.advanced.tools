cd24b45fdf7f0b393e9d825e9d792b5083657125
Guillaume Plante <guillaume.plante@icloud.com>
2025-05-29 21:20:50 -0400

    update

--- scripts/Decode.ps1
+++ scripts/Decode.ps1
@@ -1,38 +1,40 @@
 
 . "$PSScriptRoot\EncodeDecode.ps1"
 
+. "$PSScriptRoot\Invoke-AesCrypter.ps1"
+
+
 function Invoke-DoDecode {
 
     [CmdletBinding(SupportsShouldProcess)]
-    param()
+    param(
+        [Parameter(Mandatory = $false, HelpMessage = "Encoding type")]
+        [ValidateSet('base64','raw')]
+        [string]$Type='base64',
+        [Parameter(Mandatory = $false, HelpMessage = "Was the source file encrypted or not")]
+        [switch]$Encrypted
+    )
 
 
     $RootPath = (Resolve-Path -Path "$PSScriptRoot\..").Path
     $DataCipherPath = Join-Path $RootPath 'data'
     $BinSrcPath = Join-Path $RootPath 'binsrc'
+    $Filename = 'recombined.rar'
+    $EncryptedFilename = $Filename + '.aes'
 
-    $SourcePackagePath = Join-Path $BinSrcPath 'bmw_installer_package.rar.aes'
-    $RecombinedPackagePath = Join-Path $BinSrcPath 'recombined.rar.aes'
 
-    Invoke-CombineSplitFiles $DataCipherPath $RecombinedPackagePath -AsString
+    $SourcePackagePath = Join-Path $BinSrcPath $Filename
+    $RecombinedEncryptedPackagePath = Join-Path $BinSrcPath $EncryptedFilename
 
+    Invoke-CombineSplitFiles $DataCipherPath $RecombinedEncryptedPackagePath -Type 'base64'
+
+    if($Encrypted){
+        Invoke-AesCrypter -Path "$RecombinedEncryptedPackagePath" -Mode 'decrypt'
+    }  
     if ([System.IO.File]::Exists("$SourcePackagePath")) {
-        $Hash1 = (Get-FileHash -Algorithm SHA1 -Path "$SourcePackagePath").Hash
-        $Hash2 = (Get-FileHash -Algorithm SHA1 -Path "$RecombinedPackagePath").Hash
-        $Equals = $Hash1 -eq $Hash2
-        $Col2 = [System.ConsoleColor]::DarkCyan
-        if (!$Equals) {
-            $Col2 = [System.ConsoleColor]::DarkRed
-        }
-        Write-Host "[$Hash1] $SourcePackagePath" -f DarkCyan
-        Write-Host "[$Hash2] $RecombinedPackagePath" -f $Col2
-    } else {
-
-        $Hash2 = (Get-FileHash -Algorithm SHA1 -Path "$RecombinedPackagePath").Hash
-
-        Write-Host "[$Hash2] $RecombinedPackagePath" -f $Col2
+        $Hash2 = (Get-FileHash -Algorithm SHA1 -Path "$SourcePackagePath").Hash
+        Write-Host "[$Hash2] $SourcePackagePath" -f DarkCyan
     }
 }
 
-
-Invoke-DoDecode
+Invoke-DoDecode -Type 'base64' -Encrypted
