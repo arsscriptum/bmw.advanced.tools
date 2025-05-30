dd37f433f3e6acc0390c6703037643ee49d5fa32
Guillaume Plante <guillaume.plante@icloud.com>
2025-05-29 21:29:09 -0400

    update

--- scripts/Encode.ps1
+++ scripts/Encode.ps1
@@ -6,26 +6,33 @@ function Invoke-DoEncode {
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory = $false, HelpMessage = "Encoding type")]
-        [ValidateSet('base64','raw')]
-        [string]$Type='base64',
+        [ValidateSet('base64', 'raw')]
+        [string]$Type = 'base64',
         [Parameter(Mandatory = $false, HelpMessage = "Was the source file encrypted or not")]
         [switch]$Encrypted
     )
+    try {
+        $RootPath = (Resolve-Path -Path "$PSScriptRoot\..").Path
+        $DataCipherPath = Join-Path $RootPath 'data'
+        $BinSrcPath = Join-Path $RootPath 'binsrc'
+        $Filename = 'bmw_installer_package.rar'
+        $EncryptedFilename = $Filename + '.aes'
+        $SourcePackagePath = (Resolve-Path -Path "$BinSrcPath\$Filename").Path 
+        if ($Encrypted) {
+            Write-Host "Encrypted Mode=Invoke-AesCrypter -Path `"$SourcePackagePath`", will need to enter password!`n`n"
+            Invoke-AesCrypter -Path "$SourcePackagePath" -Mode 'encrypt'
+        }
 
+        [System.IO.Directory]::CreateDirectory($DataCipherPath)
 
-    $RootPath = (Resolve-Path -Path "$PSScriptRoot\..").Path
-    $DataCipherPath = Join-Path $RootPath 'data'
-    $BinSrcPath = Join-Path $RootPath 'binsrc'
-    $Filename = 'bmw_installer_package.rar'
-    $EncryptedFilename = $Filename + '.aes'
-    $SourcePackagePath = Join-Path $BinSrcPath $Filename
-    if($Encrypted){
-        Invoke-AesCrypter -Path "$SourcePackagePath" -Mode 'encrypt'
-    }  
+        $EncryptedPackagePath = (Resolve-Path -Path "$BinSrcPath\$EncryptedFilename").Path 
 
-    [System.IO.Directory]::CreateDirectory($DataCipherPath)
+        Write-Host "SplitDataFile `"$EncryptedPackagePath`"`n`n"
 
-    Invoke-SplitDataFile -Path "$SourcePackagePath" -Newsize 1Mb -OutPath "$DataCipherPath"
+        Invoke-SplitDataFile -Path "$EncryptedPackagePath" -Newsize 1Mb -OutPath "$DataCipherPath"
+    } catch {
+        Write-Error "$_"
+    }
 
 }
 
