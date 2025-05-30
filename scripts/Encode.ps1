a4bd90f6b6993f8e9e201bc6709a234692661eb7
Guillaume Plante <guillaume.plante@icloud.com>
2025-05-29 21:20:54 -0400

    update

--- scripts/Encode.ps1
+++ scripts/Encode.ps1
@@ -4,20 +4,30 @@
 function Invoke-DoEncode {
 
     [CmdletBinding(SupportsShouldProcess)]
-    param()
+    param(
+        [Parameter(Mandatory = $false, HelpMessage = "Encoding type")]
+        [ValidateSet('base64','raw')]
+        [string]$Type='base64',
+        [Parameter(Mandatory = $false, HelpMessage = "Was the source file encrypted or not")]
+        [switch]$Encrypted
+    )
 
 
     $RootPath = (Resolve-Path -Path "$PSScriptRoot\..").Path
     $DataCipherPath = Join-Path $RootPath 'data'
     $BinSrcPath = Join-Path $RootPath 'binsrc'
-    $SourcePackagePath = Join-Path $BinSrcPath 'bmw_installer_package.rar.aes'
-    $RecombinedPackagePath = Join-Path $BinSrcPath 'recombined.rar.aes'
+    $Filename = 'bmw_installer_package.rar'
+    $EncryptedFilename = $Filename + '.aes'
+    $SourcePackagePath = Join-Path $BinSrcPath $Filename
+    if($Encrypted){
+        Invoke-AesCrypter -Path "$SourcePackagePath" -Mode 'encrypt'
+    }  
 
     [System.IO.Directory]::CreateDirectory($DataCipherPath)
 
-    Invoke-SplitDataFile -Path "$SourcePackagePath" -Newsize 1Mb -OutPath "$DataCipherPath" -AsString
+    Invoke-SplitDataFile -Path "$SourcePackagePath" -Newsize 1Mb -OutPath "$DataCipherPath"
 
 }
 
 
-Invoke-DoEncode
+Invoke-DoEncode -Encrypted
